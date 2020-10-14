ed2929769ea185574ef78a537649d5cd460ab5c6
Kevin Lewis <kevin.k.lewis@gmail.com>
2020-10-14 07:37:20 -0500

    Add rule for concert

--- elixir/lib/item_rule.ex
+++ elixir/lib/item_rule.ex
@@ -26,49 +26,32 @@ defmodule ItemRule do
     end
   end
 
-  def update_item(item) do
+  def update_item(%Item{name: "Backstage passes to a TAFKAL80ETC concert"} = item) do
     item =
       cond do
-        item.name != "Backstage passes to a TAFKAL80ETC concert" ->
-          if item.quality > 0 do
-            if item.name != "Sulfuras, Hand of Ragnaros" do
-              %{item | quality: item.quality - 1}
-            else
-              item
-            end
-          else
-            item
-          end
+        item.quality < 50 ->
+          item = %{item | quality: item.quality + 1}
 
-        true ->
-          cond do
-            item.quality < 50 ->
-              item = %{item | quality: item.quality + 1}
-
-              item =
+          item =
+            cond do
+              item.sell_in < 11 ->
                 cond do
-                  item.sell_in < 11 ->
-                    cond do
-                      item.quality < 50 ->
-                        %{item | quality: item.quality + 1}
-
-                      true ->
-                        item
-                    end
+                  item.quality < 50 ->
+                    %{item | quality: item.quality + 1}
 
                   true ->
                     item
                 end
 
-              cond do
-                item.sell_in < 6 ->
-                  cond do
-                    item.quality < 50 ->
-                      %{item | quality: item.quality + 1}
+              true ->
+                item
+            end
 
-                    true ->
-                      item
-                  end
+          cond do
+            item.sell_in < 6 ->
+              cond do
+                item.quality < 50 ->
+                  %{item | quality: item.quality + 1}
 
                 true ->
                   item
@@ -77,6 +60,32 @@ defmodule ItemRule do
             true ->
               item
           end
+
+        true ->
+          item
+      end
+
+    item = %{item | sell_in: item.sell_in - 1}
+
+    cond do
+      item.sell_in < 0 ->
+        %{item | quality: item.quality - item.quality}
+
+      true ->
+        item
+    end
+  end
+
+  def update_item(item) do
+    item =
+      if item.quality > 0 do
+        if item.name != "Sulfuras, Hand of Ragnaros" do
+          %{item | quality: item.quality - 1}
+        else
+          item
+        end
+      else
+        item
       end
 
     item =
@@ -91,23 +100,17 @@ defmodule ItemRule do
     cond do
       item.sell_in < 0 ->
         cond do
-          item.name != "Backstage passes to a TAFKAL80ETC concert" ->
+          item.quality > 0 ->
             cond do
-              item.quality > 0 ->
-                cond do
-                  item.name != "Sulfuras, Hand of Ragnaros" ->
-                    %{item | quality: item.quality - 1}
-
-                  true ->
-                    item
-                end
+              item.name != "Sulfuras, Hand of Ragnaros" ->
+                %{item | quality: item.quality - 1}
 
               true ->
                 item
             end
 
           true ->
-            %{item | quality: item.quality - item.quality}
+            item
         end
 
       true ->
