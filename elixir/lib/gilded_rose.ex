46ce7c0badcf8a47e59f6b48006f525089645bec
Kevin Lewis <kevin.k.lewis@gmail.com>
2020-10-13 20:59:38 -0500

    Apply formatter

--- elixir/lib/gilded_rose.ex
+++ elixir/lib/gilded_rose.ex
@@ -8,51 +8,73 @@ defmodule GildedRose do
   end
 
   def update_item(item) do
-    item = cond do
-      item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" ->
-        if item.quality > 0 do
-          if item.name != "Sulfuras, Hand of Ragnaros" do
-            %{item | quality: item.quality - 1}
+    item =
+      cond do
+        item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" ->
+          if item.quality > 0 do
+            if item.name != "Sulfuras, Hand of Ragnaros" do
+              %{item | quality: item.quality - 1}
+            else
+              item
+            end
           else
             item
           end
-        else
+
+        true ->
+          cond do
+            item.quality < 50 ->
+              item = %{item | quality: item.quality + 1}
+
+              cond do
+                item.name == "Backstage passes to a TAFKAL80ETC concert" ->
+                  item =
+                    cond do
+                      item.sell_in < 11 ->
+                        cond do
+                          item.quality < 50 ->
+                            %{item | quality: item.quality + 1}
+
+                          true ->
+                            item
+                        end
+
+                      true ->
+                        item
+                    end
+
+                  cond do
+                    item.sell_in < 6 ->
+                      cond do
+                        item.quality < 50 ->
+                          %{item | quality: item.quality + 1}
+
+                        true ->
+                          item
+                      end
+
+                    true ->
+                      item
+                  end
+
+                true ->
+                  item
+              end
+
+            true ->
+              item
+          end
+      end
+
+    item =
+      cond do
+        item.name != "Sulfuras, Hand of Ragnaros" ->
+          %{item | sell_in: item.sell_in - 1}
+
+        true ->
           item
-        end
-      true ->
-        cond do
-          item.quality < 50 ->
-            item = %{item | quality: item.quality + 1}
-            cond do
-              item.name == "Backstage passes to a TAFKAL80ETC concert" ->
-                item = cond do
-                  item.sell_in < 11 ->
-                    cond do
-                      item.quality < 50 ->
-                        %{item | quality: item.quality + 1}
-                      true -> item
-                    end
-                  true -> item
-                end
-                cond do
-                  item.sell_in < 6 ->
-                    cond do
-                      item.quality < 50 ->
-                        %{item | quality: item.quality + 1}
-                      true -> item
-                    end
-                  true -> item
-                end
-              true -> item
-            end
-          true -> item
-        end
-    end
-    item = cond do
-      item.name != "Sulfuras, Hand of Ragnaros" ->
-        %{item | sell_in: item.sell_in - 1}
-      true -> item
-    end
+      end
+
     cond do
       item.sell_in < 0 ->
         cond do
@@ -64,20 +86,31 @@ defmodule GildedRose do
                     cond do
                       item.name != "Sulfuras, Hand of Ragnaros" ->
                         %{item | quality: item.quality - 1}
-                      true -> item
+
+                      true ->
+                        item
                     end
-                  true -> item
+
+                  true ->
+                    item
                 end
-              true -> %{item | quality: item.quality - item.quality}
+
+              true ->
+                %{item | quality: item.quality - item.quality}
             end
+
           true ->
             cond do
               item.quality < 50 ->
                 %{item | quality: item.quality + 1}
-              true -> item
+
+              true ->
+                item
             end
         end
-      true -> item
+
+      true ->
+        item
     end
   end
 end
--- elixir/mix.exs
+++ elixir/mix.exs
@@ -2,8 +2,6 @@ defmodule GildedRose.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :gilded_rose,
-     version: "0.0.1",
-     elixir: "~> 1.10"]
+    [app: :gilded_rose, version: "0.0.1", elixir: "~> 1.10"]
   end
 end
